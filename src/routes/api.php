dc83d57faaf9cdbc43c983100f3e09b00e2e22eb
Sergey <ssenokosov@justcoded.co>
2020-03-19 12:15:02 +0200

    Task 5-7: create callback endpoint with 204 response

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::put('callback', function (Request $request) {
+    return new \GuzzleHttp\Psr7\Response(204);
 });
